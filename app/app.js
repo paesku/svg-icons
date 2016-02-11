e605949fbe8287d35e8864b41f65a1e1ff797749
paesku <paesku@gmail.com>
2016-02-11 11:41:31 +0100

    Add template cache

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 (function(root, angular, undefined) {
-  
+
   angular
     .module('app', ['ngMaterial'])
     .config(function($mdIconProvider) {
@@ -7,5 +7,20 @@
          .icon('ui:cake', 'app/icons/cake.svg', 24)
          .iconSet('uiplus', 'app/icons/iconset.svg', 64)
          .iconSet('device', 'app/icons/device-icons.svg', 24);
+    })
+    .run(function($http, $templateCache) {
+
+      var urls = [
+        'app/icons/cake.svg',
+        'app/icons/iconset.svg',
+        'app/icons/device-icons.svg'
+      ];
+
+    // Pre-fetch icons sources by URL and cache in the $templateCache...
+    // subsequent $http calls will look there first.
+    angular.forEach(urls, function(url) {
+      $http.get(url, {cache: $templateCache});
     });
+
+  });
 })(this, angular);

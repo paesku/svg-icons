b8d39a5e92d05f9d6bd0f18190c95f6d82222b5f
paesku <paesku@gmail.com>
2016-02-12 16:45:20 +0100

    Document the basics and fiddle around

--- app/app.js
+++ app/app.js
@@ -12,7 +12,8 @@
       $mdIconProvider
          .icon('ui:cake', 'app/icons/cake.svg', 24)
          .iconSet('uiplus', 'app/icons/iconset.svg', 64)
-         .iconSet('device', 'app/icons/device-icons.svg', 24);
+         .iconSet('device', 'app/icons/device-icons.svg', 24)
+         .iconSet('symbols', 'app/icons/symbol-icons.svg', 60);
     })
     .run(function($http, $templateCache) {
 

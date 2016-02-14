967f0b3481108c32f014ae52d9189e092feaae16
paesku <paesku@gmail.com>
2016-02-14 20:11:28 +0100

    digg in the mud of coloring in angular material

--- app/js/app.js
+++ app/js/app.js
@@ -5,12 +5,42 @@
       'ngMaterial',
       'md-colors'
     ])
-    .controller('AppCtrl', function DemoCtrl($mdDialog) {
+    .constant('colors', {
+      'default': {
+        '50': 'ffebee',
+        '100': 'ffcdd2',
+        '200': 'ef9a9a',
+        '300': 'e57373',
+        '400': 'ef5350',
+        '500': 'f44336',
+        '600': 'e53935',
+        '700': 'd32f2f',
+        '800': 'c62828',
+        '900': 'b71c1c',
+        'A100': 'ff8a80',
+        'A200': 'ff5252',
+        'A400': 'ff1744',
+        'A700': 'd50000',
+        'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                            // on this palette should be dark or light
+        'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+         '200', '300', '400', 'A100'],
+        'contrastLightColors': undefined    // could also specify this if default was 'dark'
+      }
+    })
+    .controller('AppCtrl', function DemoCtrl($mdDialog, colors) {
       this.openMenu = function($mdOpenMenu, ev) {
         $mdOpenMenu(ev);
       };
+
+      this.active = true;
+      var  activeColor = 'rgb(' + colors.default['50'].value + ')';
+      this.activeItem = {
+         'background-color': activeColor
+      }
     })
-    .config(function($mdThemingProvider) {
+    .config(function($mdThemingProvider, colors) {
+      <!-- https://material.angularjs.org/latest/Theming/03_configuring_a_theme -->
       var customBlueMap = 		$mdThemingProvider.extendPalette('light-blue', {
         'contrastDefaultColor': 'light',
         'contrastDarkColors': ['50', '100'],
@@ -19,11 +49,14 @@
       var customGreyMap = 		$mdThemingProvider.extendPalette('blue-grey', {
         '50': 'fafafa'
       });
+      // If defining a complete palette all values must be set
+      $mdThemingProvider.definePalette('custom-palette', colors.default);
+      // Manipulate smaller pieces of a palette
       // $mdThemingProvider.definePalette('customBlue', customBlueMap);
       // $mdThemingProvider.definePalette('customGrey', customGreyMap);
       $mdThemingProvider.theme('default')
         .primaryPalette('blue-grey')
-        .accentPalette('light-blue')
+        .accentPalette('custom-palette')
         .warnPalette('orange')
         .backgroundPalette('grey', {
           'hue-1': '200',
